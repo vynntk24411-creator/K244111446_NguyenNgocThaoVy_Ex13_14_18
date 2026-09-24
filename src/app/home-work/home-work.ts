0bbb9866afd15bac7c2ef7c792107e92a97eb22e
vynntk24411-creator <vynntk24411@st.uel.edu.vn>
2026-09-24 22:45:19 +0700

    Delete src/app/home-work directory

--- src/app/home-work/home-work.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home-work',
-  standalone: false,
-  styleUrl: './home-work.css',
-  templateUrl: './home-work.html',
-})
-export class HomeWork {}
